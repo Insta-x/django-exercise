19fee4c85a67a022475c4ec1774bcf070a2a3df2
Insta-x <uttsadajason@outlook.com>
2022-09-01 21:48:14 +0700

    replace test

--- django_exercise/urls.py
+++ django_exercise/urls.py
@@ -1,4 +1,4 @@
-"""testing_django_instax URL Configuration
+"""django_exercise URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
